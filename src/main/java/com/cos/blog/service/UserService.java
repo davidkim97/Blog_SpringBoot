a6be35eebdbde03245610c698b64a1027dc19042
davidkim97 <davidmim99@gmail.com>
2021-11-07 02:10:26 +0900

    회원가입 완료

---
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -0,0 +1,27 @@
+package com.cos.blog.service;
+
+import com.cos.blog.model.User;
+import com.cos.blog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+// 스프링이 컴포넌트 스캔을 통해서 Bean에 등록을 해줌. Ioc를 해준다.
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    public int 회원가입(User user) {
+        try {
+            userRepository.save(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("UserService: 회원가입():"+e.getMessage());
+        }
+        return -1;
+    }
+}
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -11,11 +11,26 @@ let index = {
             username: $("#username").val(),
             password: $("#password").val(),
             email: $("#email").val(),
-        }
+        };
 
         //console.log(data);
         
-        $.ajax().done.fail(); // ajax 통신을 이용해서 3개의 데이터를 json 으로 변경하여 insert 요청
+        // ajax 호출시 default가 비동기 호출
+        // ajax 통신을 이용해서 3개의 데이터를 json 으로 변경하여 insert 요청
+        // ajax가 통신을 성공하고 서버가 json을 리턴해주면 자동으로 자바 오브젝트로 변환해줌.
+        $.ajax({
+            type: "POST",
+            url: "/blog/api/user",
+            data: JSON.stringify(data), // http body 데이터
+            contentType: "application/json; charset=utf-8", // body 데이터가 어떤 타입인지(MIME)
+            dataType: "json"// 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json 이라면) => javascript 오브젝트로 변경
+        }).done(function (resp){
+            alert("회원가입이 완료되었습니다.");
+            //console.log(resp);
+            location.href = "/blog";
+        }).fail(function (error){
+            alert(JSON.stringify(error));
+        });
     }
 }
 

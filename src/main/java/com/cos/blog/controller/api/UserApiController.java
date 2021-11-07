45a11657b8c913e6bc758ac03df018757ece7382
davidkim97 <davidmim99@gmail.com>
2021-11-07 11:48:35 +0900

    ResponseDto 수정

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -20,7 +20,7 @@ public class UserApiController {
     public ResponseDto<Integer> save(@RequestBody User user) { // username, password, email
         System.out.println("UserApiController : save 호출됨");
         user.setRole(RoleType.USER);
-        int result = userService.회원가입(user);
-        return new ResponseDto<Integer>(HttpStatus.OK, result); // 자바오브젝트를 JSON으로 변환해서 리턴(Jackson)
+        userService.회원가입(user);
+        return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); // 자바오브젝트를 JSON으로 변환해서 리턴(Jackson)
     }
 }
